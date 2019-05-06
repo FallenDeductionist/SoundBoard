07012add886afc3dadb3e1abc98bf4f42ff8eb84
Mario Fernando <mario.paucar@tecsup.edu.pe>
2019-05-06 13:56:23 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by Mario Fernando Paucar Gutierrez on 5/6/19.
+//  Copyright © 2019 Tecsup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
